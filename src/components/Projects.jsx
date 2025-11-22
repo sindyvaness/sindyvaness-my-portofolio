56d2ebbbd87478e40c91638dffe25f98e8f7697c
sindyvaness <42687025+sindyvaness@users.noreply.github.com>
2025-11-22 13:51:56 -0800

    Adding assets for movies-app project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,7 +4,34 @@ const Projects = () => {
     return(
         <div className="projects">
             <h2 className="subtitles">My Work</h2>
-            <div>
+            <div className="projects-container">
+                <div className="card project-item">
+                    <img src={carrousel1} className="card-img-top" alt="..." />
+                    <div className="card-body">
+                        <h5 className="card-title">Card title</h5>
+                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" className="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+
+                <div className="card project-item">
+                    <img src={carrousel1} className="card-img-top" alt="..." />
+                    <div className="card-body">
+                        <h5 className="card-title">Card title</h5>
+                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" className="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+
+                <div className="card project-item">
+                    <img src={carrousel1} className="card-img-top" alt="..." />
+                    <div className="card-body">
+                        <h5 className="card-title">Card title</h5>
+                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" className="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+
                 <div className="card project-item">
                     <img src={carrousel1} className="card-img-top" alt="..." />
                     <div className="card-body">
