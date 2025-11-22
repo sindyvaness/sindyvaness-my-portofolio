0abc2722459a14b1249d507a408758de8e65fcfe
sindyvaness <42687025+sindyvaness@users.noreply.github.com>
2025-11-22 15:42:24 -0800

    Creating responsive design. Creating the Project container files.

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,16 +1,19 @@
+import { useState } from "react";
 import carrousel1 from "../assets/images/hero/carrousel-1.jpg";
 
 const Projects = () => {
+    const [showModal, setShowModal] = useState(false);
+
     return(
         <div className="projects">
             <h2 className="subtitles">My Work</h2>
-            <div className="projects-container">
+            <div className="d-flex justify-content-between flex-wrap row-gap-3 projects-container">
                 <div className="card project-item">
                     <img src={carrousel1} className="card-img-top" alt="..." />
                     <div className="card-body">
                         <h5 className="card-title">Card title</h5>
                         <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" className="btn btn-primary">Go somewhere</a>
+                        <a className="btn btn-primary" onClick={() => setShowModal(true)}>Go somewhere</a>
                     </div>
                 </div>
 
