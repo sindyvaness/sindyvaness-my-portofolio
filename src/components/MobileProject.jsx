35ed1319abda553ec0dc45824455f394fa45c181
sindyvaness <42687025+sindyvaness@users.noreply.github.com>
2025-11-27 11:39:41 -0800

    Porject Mobile: improvements in the layout and design.

--- src/components/MobileProject.jsx
+++ src/components/MobileProject.jsx
@@ -15,8 +15,7 @@ const MobileProject = () => {
                 </div>
                 <div className="mbp-description">
                     <div className="mbp-title">Movies App - React Native - Movies API</div>
-                    <p><i class="bi bi-file-earmark-text"></i><strong>Description:</strong>
-                        A movie and TV show browsing application that fetches up-to-date content from a real-time movie API. Users can explore trending titles, search for specific films or series, and view detailed metadata including ratings, descriptions, cast information, and more.
+                    <p>A movie and TV show browsing application that fetches up-to-date content from a real-time movie API. Users can explore trending titles, search for specific films or series, and view detailed metadata including ratings, descriptions, cast information, and more.
                     </p>
                     <p><i class="bi bi-code-square"></i><strong>Technologies:</strong></p>
                     <ul>
@@ -25,14 +24,12 @@ const MobileProject = () => {
                         <li>React Native Safe Area Context for handling safe areas</li>
                         <li>React Navigation for app navigation</li>
                     </ul>
-                    <p>External Resources: 
-                        <a 
-                            href="https://www.themoviedb.org/" 
-                            target="_blank" 
-                            rel="noopener noreferrer"
-                            >
-                            The Movie BD
-                        </a>
+                    <p><i class="bi bi-cloud-download"></i><strong>External Resources:</strong>
+                        <ul>
+                            <li>
+                                <a href="https://www.themoviedb.org/" target="_blank" rel="noopener noreferrer">The Movie BD</a>
+                            </li>
+                        </ul>
                     </p>
                     <p><i class="bi bi-gear"></i><strong>Main Features:</strong></p>
                     <ul>
@@ -42,23 +39,23 @@ const MobileProject = () => {
                         <li>See movie description and metadata such as title, image, popularity, and release date</li>
                     </ul>
                     <div>
-                        <p>Feature Features and improvements:</p>
+                        <p><i class="bi bi-lightning"></i><strong>Feature Features and improvements:</strong></p>
                         <ul>
                             <li>General Layout and design.</li>
                             <li>Fetch more info about the movie.</li>
                             <li>Format of the texts in the select boxes.</li>
-                            <li></li>
                         </ul>
                     </div>
                     <div>
-                        <i class="bi bi-github"></i>  
-                        <a 
-                            href="https://github.com/sindyvaness/react-native-movie-app" 
-                            target="_blank" 
-                            rel="noopener noreferrer"
-                            >
-                            Movies App - GitHub Repo
-                        </a>
+                        <p><i class="bi bi-github"></i>
+                            <strong><a 
+                                href="https://github.com/sindyvaness/react-native-movie-app" 
+                                target="_blank" 
+                                rel="noopener noreferrer"
+                                >
+                                Movies App - GitHub Repo
+                            </a></strong>
+                        </p>
                     </div>
                 </div>
             </div>
